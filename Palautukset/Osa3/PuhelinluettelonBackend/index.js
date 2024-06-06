b4680d18aaab42f355f7076809072bd2dd686325
Lapess <147448169+Lapess@users.noreply.github.com>
2024-06-07 01:00:02 +0300

    3.22 done, ready for submission

--- Palautukset/Osa3/PuhelinluettelonBackend/index.js
+++ Palautukset/Osa3/PuhelinluettelonBackend/index.js
@@ -1,104 +1,112 @@
-require('dotenv').config()
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
 
-const Person = require("./models/person")
+const Person = require("./models/person");
 
-morgan.token("content", (req => JSON.stringify(req.body)))
+morgan.token("content", (req) => JSON.stringify(req.body));
 
-app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content' ))
-app.use(cors())
-app.use(express.static("dist"))
+app.use(express.json());
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :content"
+  )
+);
+app.use(cors());
+app.use(express.static("dist"));
 
-
-
-app.get("/api/persons", (request, response, next)=>{
-    Person.find({}).then( persons =>{
-        response.json(persons)
+app.get("/api/persons", (request, response, next) => {
+  Person.find({})
+    .then((persons) => {
+      response.json(persons);
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
-app.get("/api/persons/:id", (request, response)=>{
+app.get("/api/persons/:id", (request, response, next) => {
+  const id = request.params.id;
+  Person.findById(id)
+    .then((person) => {
+      response.json(person);
 
-    const id = request.params.id
-    Person.findById(id)
-    .then(person =>{
-        response.json(person)
-
-    if (person === undefined || person === null){
-        response.status(404).end()
-    }
+      if (person === undefined || person === null) {
+        response.status(404).end();
+      }
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
-app.delete("/api/persons/:id", (request,response, next)=>{
-    Person.findByIdAndDelete(request.params.id)
-    .then(result =>{
-        response.status(204).end()
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end();
     })
-    .catch(error => next(error))
-  })
+    .catch((error) => next(error));
+});
 
-app.post("/api/persons",(request, response, next)=>{
-    const body = request.body
+app.post("/api/persons", (request, response, next) => {
+  const body = request.body;
 
-    if (body.name === "" || body.number === ""){
-       return response.status(400).json({ "error": "Invalid parameters. Request must contain json with name(str) and number(str)"})
-        }
+  if (body.name === "" || body.number === "") {
+    return response.status(400).json({
+      error:
+        "Invalid parameters. Request must contain json with name(str) and number(str)",
+    });
+  }
 
-    const person = new Person ({
-        name : body.name,
-        number: body.number
-        })
-        person.save().then(savedperson =>{
-            response.json(savedperson)
-        })
-    
-    .catch(error => next(error))
-})
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+  person
+    .save()
+    .then((savedperson) => {
+      response.json(savedperson);
+    })
 
-app.put('/api/persons/:id', (request, response, next) => {
-    const {name, number} = request.body
-  
-    Person.findByIdAndUpdate(
-        request.params.id,
-        {name, number},
-        //performs validation for update method parameters. Currently not needed, might be handy in future?
-        {new: true, runValidators: true, context: "query"})
-      .then(updatedPerson => {
-        response.json(updatedPerson)
-      })
-      .catch(error => next(error))
-  })
+    .catch((error) => next(error));
+});
 
-app.get("/info",(request, response, next)=>{
-    Person.find({})
-    .then(persons =>{
-        response.send(`<div>Phonebook has info for ${persons.length} people</div>
+app.put("/api/persons/:id", (request, response, next) => {
+  const { name, number } = request.body;
+
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    //performs validation for update method parameters. Currently not needed, might be handy in future?
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+app.get("/info", (request, response, next) => {
+  Person.find({})
+    .then((persons) => {
+      response.send(`<div>Phonebook has info for ${persons.length} people</div>
         <div>
         ${new Date().toString()}
-        </div>`)
+        </div>`);
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
-const errorHandler = (error, request, response, next)=>{
-console.log(error.message)
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
 
-if (error.name === "ValidationError"){
-    response.status(400).json({error: error.message})
-}
+  if (error.name === "ValidationError") {
+    response.status(400).json({ error: error.message });
+  }
 
-next(error)
-}
+  next(error);
+};
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-const PORT = process.env.PORT
-app.listen(PORT)
-console.log(`Server started succesfully on port ${PORT}`)
+const PORT = process.env.PORT;
+app.listen(PORT);
+console.log(`Server started succesfully on port ${PORT}`);
--- Palautukset/Osa3/PuhelinluettelonBackend/models/person.js
+++ Palautukset/Osa3/PuhelinluettelonBackend/models/person.js
@@ -1,64 +1,61 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-mongoose.set("strictQuery", false)
+mongoose.set("strictQuery", false);
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
-console.log("Connecting to database")
-mongoose.connect(url)
-.then(() =>{
-    console.log("connection established to database succesfully")
-})
-.catch(error=>{
-    console.log("Error occured while connecting to database: ", error.message)
-})
-
+console.log("Connecting to database");
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("connection established to database succesfully");
+  })
+  .catch((error) => {
+    console.log("Error occured while connecting to database: ", error.message);
+  });
 
 const contactSchema = new mongoose.Schema({
-    name:{
-     type: String,
-     minlength: 3
+  name: {
+    type: String,
+    minlength: 3,
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: (val) => {
+        if (val.length >= 8) {
+          let count = 0;
+          for (let i = 0; i < val.length; i++) {
+            if (val[i] === "-") {
+              count++;
+            }
+          }
+          if (count !== 1) {
+            return false;
+          } else {
+            const valparts = val.split("-");
+            if (valparts[0].length > 3 || valparts[0].length < 2) {
+              return false;
+            } else {
+              return true;
+            }
+          }
+        } else {
+          return false;
+        }
+      },
+      message: (number) =>
+        `${number.value} is in invalid. number must be atleast 8 characters long and in xx-xxxxx or xxx-xxxx format`,
     },
-    number:{
-      type : String,
-      validate:{
-        validator:(val)=>{
-          if (val.length >= 8){
-            let count = 0
-            for (let i = 0; i < val.length; i++){
-              if (val[i] === "-"){
-                count++
-              }
-            }
-            if (count !== 1){
-              return false
-            }
-            else{
-              const valparts = val.split("-")
-              if (valparts[0].length > 3 || valparts[0].length < 2){
-                return false
-              }
-              else{
-                return true
-              }
-            }
-          }
-          else{
-            return false
-          }
-        },
-        message: number => `${number.value} is in invalid. number must be atleast 8 characters long and in xx-xxxxx or xxx-xxxx format`
-      }
-    }
-})
+  },
+});
 
-contactSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+contactSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Person', contactSchema)
-
+module.exports = mongoose.model("Person", contactSchema);
